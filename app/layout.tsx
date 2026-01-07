9529b89a7fcce090a0abb1dcf61a8c53675f7901
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-07 14:54:41 +0400

    Add Prettier integration and update global styles
    - Integrated Prettier with ESLint for consistent code formatting.
    - Replaced font imports with Inter and Poppins.
    - Adjusted global CSS to streamline theme management.
    - Added utility function for class management.
    - Updated `next.config.ts`, `tsconfig.json`, and Tailwind CSS configuration.
    - Created `components.json` file with project-specific aliases and settings.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppinsSans = Poppins({
+  variable: "--font-poppins-sans",
   subsets: ["latin"],
+  weight: "300",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.variable} ${poppinsSans.variable} antialiased`}
       >
         {children}
       </body>
